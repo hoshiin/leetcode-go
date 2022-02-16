d70341bf23e811882ff7a5c60b6057896a0f3cc9
Hoshii Natsuki <hoshii.natsuki@hrbrain.co.jp>
2022-02-16 23:19:45 +0900

    Add a solution for 0053 with recursion

--- leetcode/0053/max_sum_array.go
+++ leetcode/0053/max_sum_array.go
@@ -9,11 +9,49 @@ func MaxSubArray(nums []int) int {
 
 	for i := 1; i < len(nums); i++ {
 		num := float64(nums[i])
-		currentSubArray = math.Max(float64(num), currentSubArray+num)
+		currentSubArray = math.Max(num, currentSubArray+num)
 		maxSubArray = math.Max(maxSubArray, currentSubArray)
 	}
 
 	return int(maxSubArray)
 }
 
-// TODO: Recursion Solution
+// MaxSubArray uses recursion
+func MaxSubArrayRecursion(nums []int) int {
+	numsArray := nums
+	return findBestSubArray(numsArray, 0, len(numsArray)-1)
+}
+
+func findBestSubArray(nums []int, left, right int) int {
+	// Base case - empty array.
+	if left > right {
+		return int(math.Inf(-1))
+	}
+
+	mid := (left + right) / 2
+	curr, bestLeftSum, bestRightSum := float64(0), float64(0), float64(0)
+
+	// Iterate from the middle to the beginning.
+	for i := mid - 1; i >= left; i-- {
+		curr += float64(nums[i])
+		bestLeftSum = math.Max(bestLeftSum, curr)
+	}
+
+	// Reset curr and iterate from the middle to the end.
+	curr = 0
+	for i := mid + 1; i <= right; i++ {
+		curr += float64(nums[i])
+		bestRightSum = math.Max(bestRightSum, curr)
+	}
+
+	// The bestCombinedSum uses the middle element and the best
+	// possible sum from each half.
+	bestCombinedSum := float64(nums[mid]) + bestLeftSum + bestRightSum
+
+	// Find the best subarray possible from both halves.
+	leftHalf := float64(findBestSubArray(nums, left, mid-1))
+	rightHalf := float64(findBestSubArray(nums, mid+1, right))
+
+	// The largest of the 3 is the answer for any given input array.
+	return int(math.Max(math.Max(bestCombinedSum, leftHalf), rightHalf))
+}

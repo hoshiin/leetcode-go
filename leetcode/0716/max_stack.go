752385b5bf787205ffb6073df8d409949eae83c6
Hoshii Natsuki <hoshii.natsuki@hrbrain.co.jp>
2022-03-06 10:23:47 +0900

    Use maxStack

--- leetcode/0716/max_stack.go
+++ leetcode/0716/max_stack.go
@@ -1,62 +1,50 @@
 package leetcode
 
-import "math"
-
 type MaxStack struct {
-	stack     []int
-	lastIndex int
-	maxIndex  int
+	stack    []int
+	maxStack []int
 }
 
 func Constructor() MaxStack {
 	return MaxStack{
-		stack:     []int{},
-		lastIndex: -1,
-		maxIndex:  -1,
+		stack:    []int{},
+		maxStack: []int{},
 	}
 }
 
 func (this *MaxStack) Push(x int) {
-	if len(this.stack) == 0 || x >= this.stack[this.maxIndex] {
-		this.maxIndex = len(this.stack)
+	if len(this.maxStack) == 0 || this.maxStack[len(this.maxStack)-1] < x {
+		this.maxStack = append(this.maxStack, x)
+	} else {
+		this.maxStack = append(this.maxStack, this.maxStack[len(this.maxStack)-1])
 	}
 	this.stack = append(this.stack, x)
-	this.lastIndex++
 }
 
 func (this *MaxStack) Pop() int {
-	if this.lastIndex == this.maxIndex {
-		return this.PopMax()
-	}
-	out := this.stack[this.lastIndex]
-	this.stack = this.stack[:this.lastIndex]
-	this.lastIndex--
+	out := this.stack[len(this.stack)-1]
+	this.stack = this.stack[:len(this.stack)-1]
+	this.maxStack = this.maxStack[:len(this.maxStack)-1]
 	return out
 }
 
 func (this *MaxStack) Top() int {
-	return this.stack[this.lastIndex]
+	return this.stack[len(this.stack)-1]
 }
 
 func (this *MaxStack) PeekMax() int {
-	return this.stack[this.maxIndex]
+	return this.maxStack[len(this.maxStack)-1]
 }
 
 func (this *MaxStack) PopMax() int {
-	out := this.stack[this.maxIndex]
-	if this.maxIndex+1 <= len(this.stack)-1 {
-		this.stack = append(this.stack[:this.maxIndex], this.stack[this.maxIndex+1:]...)
-	} else {
-		this.stack = this.stack[:this.maxIndex]
+	buffer := []int{}
+	max := this.PeekMax()
+	for max != this.Top() {
+		buffer = append(buffer, this.Pop())
 	}
-	this.lastIndex--
-	max := int(math.Inf(-1))
-	for i, s := range this.stack {
-		if s >= max {
-			max = s
-			this.maxIndex = i
-		}
+	this.Pop()
+	for i := len(buffer) - 1; i >= 0; i-- {
+		this.Push(buffer[i])
 	}
-
-	return out
+	return max
 }

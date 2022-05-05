c7c29fd08a9b95c3916d5663063382a029d7ab02
Hoshii Natsuki <hoshii.natsuki@hrbrain.co.jp>
2022-05-05 21:38:34 +0900

    Add 0116

---
+++ leetcode/0116/connect.go
@@ -0,0 +1,52 @@
+package leetcode
+
+type Node struct {
+	Val   int
+	Left  *Node
+	Right *Node
+	Next  *Node
+}
+
+func Connect(root *Node) *Node {
+	if root == nil {
+		return root
+	}
+	queue := []*Node{root}
+	for len(queue) > 0 {
+		size := len(queue)
+		for i := 0; i < size; i++ {
+			node := queue[0]
+			queue = queue[1:]
+			if i < size-1 {
+				node.Next = queue[0]
+			}
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+
+	}
+	return root
+}
+
+func ConnectUsePointer(root *Node) *Node {
+	if root == nil {
+		return root
+	}
+	leftmost := root
+	for leftmost.Left != nil {
+		head := leftmost
+		for head != nil {
+			head.Left.Next = head.Right
+			if head.Next != nil {
+				head.Right.Next = head.Next.Left
+			}
+			head = head.Next
+		}
+		leftmost = leftmost.Left
+	}
+	return root
+}

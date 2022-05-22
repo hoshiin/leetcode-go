9b0d0749ee57a0b627859a467cacc1a6118a9b6f
Hoshii Natsuki <hoshii.natsuki@hrbrain.co.jp>
2022-05-23 00:01:08 +0900

    Add Bottom-up solution in 1770

--- leetcode/1770/maximum_score.go
+++ leetcode/1770/maximum_score.go
@@ -26,3 +26,20 @@ func max(a, b int) int {
 	}
 	return b
 }
+
+func MaximumScoreBottomUp(nums []int, multipliers []int) int {
+	n := len(nums)
+	m := len(multipliers)
+	dp := make([][]int, m+1)
+	for i, _ := range dp {
+		dp[i] = make([]int, m+1)
+	}
+	for i := m - 1; i >= 0; i-- {
+		for left := i; left >= 0; left-- {
+			mult := multipliers[i]
+			right := n - 1 - (i - left)
+			dp[i][left] = max(mult*nums[left]+dp[i+1][left+1], mult*nums[right]+dp[i+1][left])
+		}
+	}
+	return dp[0][0]
+}

00b935c7a1795d541f64b6bec41cc872894a2e7a
Hoshii Natsuki <hoshii.natsuki@hrbrain.co.jp>
2022-05-26 00:34:33 +0900

    Add top-down solution in 0139

--- leetcode/0139/word_break.go
+++ leetcode/0139/word_break.go
@@ -14,3 +14,30 @@ func WordBreak(s string, wordDict []string) bool {
 	}
 	return dp[len(s)-1]
 }
+
+var memo []int
+
+func WordBreakTopDown(s string, wordDict []string) bool {
+	memo = make([]int, len(s))
+	return dp(len(s)-1, s, wordDict)
+}
+
+func dp(i int, s string, wordDict []string) bool {
+	if i < 0 {
+		return true
+	}
+	if memo[i] == 0 {
+		for _, word := range wordDict {
+			if i >= len(word)-1 && dp(i-len(word), s, wordDict) {
+				if string(s[i-len(word)+1:i+1]) == word {
+					memo[i] = 1
+					break
+				}
+			}
+		}
+	}
+	if memo[i] == 0 {
+		memo[i] = -1
+	}
+	return memo[i] == 1
+}

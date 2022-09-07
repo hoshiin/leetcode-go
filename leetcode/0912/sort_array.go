fc0f1021f986270af69a9b774e51168f27a2c817
Hoshii Natsuki <hoshii.natsuki@hrbrain.co.jp>
2022-09-07 23:07:55 +0900

    Add quick sort solution in 0912

--- leetcode/0912/sort_array.go
+++ leetcode/0912/sort_array.go
@@ -29,3 +29,30 @@ func merge(a, b []int) []int {
 	}
 	return ans
 }
+
+func SortArrayQuickSort(nums []int) []int {
+	quickSort(nums, 0, len(nums)-1)
+	return nums
+}
+
+func quickSort(arr []int, low, high int) {
+	if low < high {
+		pivot := partition(arr, low, high)
+		quickSort(arr, low, pivot-1)
+		quickSort(arr, pivot+1, high)
+	}
+
+}
+
+func partition(arr []int, low, high int) int {
+	pivot := arr[high]
+	i := low
+	for j := low; j < high; j++ {
+		if arr[j] < pivot {
+			arr[i], arr[j] = arr[j], arr[i]
+			i++
+		}
+	}
+	arr[i], arr[high] = arr[high], arr[i]
+	return i
+}

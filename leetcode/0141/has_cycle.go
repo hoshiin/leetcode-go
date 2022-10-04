be0266067c3a56cfc6da7c6ce7b4260865c42f94
Hoshii Natsuki <hoshii.natsuki@hrbrain.co.jp>
2022-10-04 09:18:29 +0900

    Add 0141

---
+++ leetcode/0141/has_cycle.go
@@ -0,0 +1,31 @@
+package leetcode
+
+import "github.com/hoshiin/leetcode-go/structures"
+
+func HasCycle(head *structures.ListNode) bool {
+	seen := map[*structures.ListNode]struct{}{}
+	for head != nil {
+		if _, ok := seen[head]; ok {
+			return true
+		}
+		seen[head] = struct{}{}
+		head = head.Next
+	}
+	return false
+}
+
+func HasCycle2(head *structures.ListNode) bool {
+	if head == nil {
+		return false
+	}
+	slow := head
+	fast := head.Next
+	for slow != fast {
+		if fast == nil || fast.Next == nil {
+			return false
+		}
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	return true
+}

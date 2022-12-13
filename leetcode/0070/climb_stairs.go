f36365bc9bbd6d9e28bd5cc1a21ddaf1daa04de6
Hoshii Natsuki <hoshii.natsuki@hrbrain.co.jp>
2022-12-13 09:12:21 +0900

    Add fibonacci solution for 0070

--- leetcode/0070/climb_stairs.go
+++ leetcode/0070/climb_stairs.go
@@ -45,3 +45,18 @@ func ClimbStairsDP(n int) int {
 	}
 	return dp[n]
 }
+
+func ClimbStairsFibo(n int) int {
+	if n == 1 {
+		return 1
+	}
+	first := 1
+	second := 2
+
+	for i := 3; i <= n; i++ {
+		third := first + second
+		first = second
+		second = third
+	}
+	return second
+}
